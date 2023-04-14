9e2e8e120d6e68068421ee9a427ebf785f71a5d5
Ivana <ivanacebova@gmail.com>
2023-04-15 01:46:23 +0200

    update the app.js, better calculation, and WHR

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ posenet.load().then(function (net) {
     // calculated height value
     var actualHeight;
 
+    // Waist-Hip Ratio
+    // average of the standard WHR values for males and females,
+    // which are 0.9 or lower for males and 0.80 or lower for females
+    // Waist Size = Hip Size * WHR
+    const WHR = 0.85;
+
     // Function to get video stream
     function getVideoStream() {
         navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
@@ -30,7 +36,6 @@ posenet.load().then(function (net) {
 
     // Function to estimate poses from video frames
     function estimatePoses() {
-        // video.addEventListener('loadedmetadata', () => {
         // Draw video frame on canvas
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
@@ -39,9 +44,6 @@ posenet.load().then(function (net) {
 
         // Convert the image data to a tensor
         const imageTensor = tf.browser.fromPixels(imageData);
-
-        // Wrap the image tensor in an array or a tensor array
-        const inputTensor = tf.tensor([imageTensor]);
 
         net.estimateSinglePose(imageTensor, {
             flipHorizontal: false, // Set to true if video is mirrored
@@ -55,10 +57,27 @@ posenet.load().then(function (net) {
             console.log(pose);
 
             // Calculate body dimensions
-            var height = pose.keypoints[10].position.y - pose.keypoints[0].position.y;
-            var waistSize = pose.keypoints[10].position.x - pose.keypoints[9].position.x;
-            var hipsSize = pose.keypoints[12].position.x - pose.keypoints[11].position.x;
-            var chestSize = pose.keypoints[14].position.x - pose.keypoints[13].position.x;
+            // KEYPOINTS
+            // 0: Nose
+            // 1: Left Eye
+            // 2: Right Eye
+            // 3: Left Ear
+            // 4: Right Ear
+            // 5: Left Shoulder
+            // 6: Right Shoulder
+            // 7: Left Elbow
+            // 8: Right Elbow
+            // 9: Left Wrist
+            // 10: Right Wrist
+            // 11: Left Hip
+            // 12: Right Hip
+            // 13: Left Knee
+            // 14: Right Knee
+            // 15: Left Ankle
+            // 16: Right Ankle
+
+            // Calculate height as the distance between nose and leftAnkle
+            var height = Math.abs(pose.keypoints[0].position.y - pose.keypoints[15].position.y);
 
             // Check if myInput has a value, otherwise use default value of 170
             actualHeight = (typeof myInput === "undefined" || myInput === null || myInput === '') ? 170 : myInput;
@@ -66,30 +85,26 @@ posenet.load().then(function (net) {
             //Conversion Factor = (Real-world Distance in cm) / (Size of Object in Pixels)
             const conversionFactor = actualHeight / height;
 
+            // Calculate waist size as the distance between leftHip and rightHip multiplied by the conversionFactor and WHR
+            var waistSize = Math.abs(pose.keypoints[12].position.x - pose.keypoints[11].position.x) * conversionFactor * WHR;
+
+            // Calculate hip size as the distance between leftHip and rightHip multiplied by the conversionFactor
+            var hipsSize = Math.abs(pose.keypoints[12].position.x - pose.keypoints[11].position.x) * conversionFactor;
+
+            // Calculate chest size as the distance between leftShoulder and rightShoulder multiplied by the conversionFactor
+            var chestSize = Math.abs(pose.keypoints[6].position.x - pose.keypoints[5].position.x) * conversionFactor;
+
             // Update the body dimensions on the web page
-            document.getElementById('height').innerText = 'Height: '
-                + (height * conversionFactor > 0
-                    ? height * conversionFactor.toFixed(2)
-                    : 0)
-                + ' cm';
+            document.getElementById('height').innerText = 'Height: ' + (height * conversionFactor).toFixed(2) + 'cm';
+            document.getElementById('waistSize').innerText = 'Waist Size: ' + waistSize.toFixed(2) + 'cm';
+            document.getElementById('hipsSize').innerText = 'Hips Size: ' + hipsSize.toFixed(2) + 'cm';
+            document.getElementById('chestSize').innerText = 'Chest Size: ' + chestSize.toFixed(2) + 'cm';
 
-            document.getElementById('waistSize').innerText = 'Waist Size: '
-                + (waistSize * conversionFactor > 0
-                    ? waistSize * conversionFactor.toFixed(2)
-                    : 0)
-                + ' cm';
-
-            document.getElementById('hipsSize').innerText = 'Hips Size: '
-                + (hipsSize * conversionFactor > 0
-                    ? hipsSize * conversionFactor.toFixed(2)
-                    : 0)
-                + ' cm';
-
-            document.getElementById('chestSize').innerText = 'Chest Size: '
-                + (chestSize * conversionFactor > 0
-                    ? chestSize * conversionFactor.toFixed(2)
-                    : 0)
-                + ' cm';
+            // Render the dimensions in your app's UI or display them in any other way you prefer
+            console.log('Height:', height * conversionFactor);
+            console.log('Waist Size:', waistSize);
+            console.log('Hip Size:', hipsSize);
+            console.log('Chest Size:', chestSize);
 
             // Call estimatePoses() again to process the next video frame
             requestAnimationFrame(estimatePoses);
